1264409160793e9390f29e4746c5084b4437d3c1
Julian Torres <julian.torres@mongodb.com>
2020-12-10 20:13:07 -0500

    better demo of the problem

--- tests/test_things.py
+++ tests/test_things.py
@@ -4,21 +4,23 @@ from jira import JIRA
 
 
 def test_get_jira_issue(mocker):
+    def __init__(self):
+        self.jira = JIRA()
+
     mocker.patch.object(JIRA, "__init__", autospec=True, return_value=None)
     mocker.patch.object(
         JIRA,
         "issue",
         return_value={"hey": "I ACTUALLY RETURNED IN THE RIGHT CALL HALLELUJAH"},
     )
-
-    def __init__(self):
-        self.jira = JIRA()
-
     mocker.patch.object(FakeJira, "__init__", __init__)
 
     hey = FakeJira()
-    the_issue_call_works_i_swear = JIRA.issue()
 
     issue = hey.get_jira_issue("some issue")
 
-    assert issue == the_issue_call_works_i_swear
+    assert issue == {
+        "code": "the call actually succeeded, but I need to mock issue.raw and idk how",
+        "issue": "",
+        "this": "never gets called",
+    }
